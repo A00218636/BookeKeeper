e6bf3d461fe0e204526c0989a875bdb9fe8a739b
prateek <A00218636@mycambrian.ca>
2021-07-31 18:30:03 -0700

    commit

---
+++ BookKeeper/Data/BookKeeperDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookKeeper.Data
+{
+    public class BookKeeperDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryType> CategoryTypes { get; set; }
+        public BookKeeperDbContext()
+        {
+        }
+    }
+}
